2be91e02cb788de98524135aa6b493587503bcc2
Mikita Besau <mikitabesau@gmail.com>
2025-11-13 18:49:30 +0200

    db setup

---
+++ TodoListApp.WebApp/Models/ToDoListDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoListApp.WebApp.Models
+{
+    public class ToDoListDbContext : DbContext
+    {
+        public ToDoListDbContext(DbContextOptions<ToDoListDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ToDoList> ToDoLists { get; set; }
+    }
+}
